30419ac1b1480f15196f32bcd01a39b0bb2c52c7
Alvin <sanghup123@naver.com>
2020-03-03 18:06:41 +0900

    fix: 🔥 file ext change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      Myway!
-    </div>
-  );
+  return <div className="App">myway!</div>;
 }
 
 export default App;
