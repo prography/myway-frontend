3dbfdd09f15f7377ef0accbfabdbf5ecb4da4cab
sanghup1234 <sanghup123@naver.com>
2020-03-31 01:25:18 +0900

    add Slide component

---
+++ src/components/Slide/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function Slide({ children }: Props) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
+  return <Slider {...settings}>{children}</Slider>;
+}
+
+export default Slide;
