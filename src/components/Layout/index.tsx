621fd0c4f38e561dff2a66f598953cfcf1fe3f6e
sanghup1234 <sanghup123@naver.com>
2020-04-07 21:43:21 +0900

    Header 컴포넌트 제거

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from './Header';
 
 const ContentWrap = styled.div`
   position: relative;
@@ -10,8 +9,7 @@ const ContentWrap = styled.div`
 export function LayoutHOC(Page: React.ComponentType) {
   return () => (
     <ContentWrap>
-      <Header />
       <Page />
     </ContentWrap>
   );
-};
+}
