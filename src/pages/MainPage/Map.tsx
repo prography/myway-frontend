50354dc60394b0826e9641dad45906e751cc5775
HeeYeon <gmldus0407@naver.com>
2020-04-24 03:33:09 +0900

    네이버 지도 API 연결

---
+++ src/pages/MainPage/Map.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+declare global {
+  interface Window {
+    naver: any; 
+  }
+}
+
+const Map = () => {
+  const { naver } = window;
+  
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const mapOptions = {
+      center: new naver.maps.LatLng(37.4999894, 127.027699),
+      zoom: 16,
+      minZoom: 6,
+      scaleControl: false,
+      logoControl: false,
+      mapDataControl: false,
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: naver.maps.MapTypeControlStyle.BUTTON,
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+      zoomControl: true,
+      zoomControlOptions: {
+        style: naver.maps.ZoomControlStyle.SMALL,
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+    };
+
+    const map = new naver.maps.Map(container, mapOptions);
+
+    const marker = new naver.maps.Marker({
+      position: mapOptions.center,
+      map,
+    });
+
+    naver.maps.Event.addListener(map, 'click', function(e : any) {
+      marker.setPosition(e.coord);
+    });
+  }, []);
+
+  return (
+    <>
+      <MapContainer id="map" />
+    </>
+  );
+}
+
+export default Map;
+
+const MapContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
