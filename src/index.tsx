8f667480c51d90e12f393435dc3e29fc6da52d84
sanghup1234 <sanghup123@naver.com>
2020-05-01 23:56:45 +0900

    스토어 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { Theme } from 'styles/theme';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
+import createStore from './store/createStore';
 
 import 'styles/fonts/fonts.css';
 import 'styles/reset.css';
 import 'styles/color.css';
 
+// store 생성
+const store = createStore();
+
 ReactDOM.render(
-  <ThemeProvider theme={Theme}>
-    <Root />
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={Theme}>
+      <Root />
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root'),
 );
 
