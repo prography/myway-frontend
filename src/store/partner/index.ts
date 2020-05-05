e50ae7db9915cb860b5defcf080e8990a88a337c
HeeYeon <gmldus0407@naver.com>
2020-05-05 13:59:41 +0900

    리듀서 수정

--- src/store/partner/index.ts
+++ src/store/partner/index.ts
@@ -16,14 +16,14 @@ const partnerReducer = (state: PartnerState = initialState, action: any): Partne
     switch (action.type) {
       case 'GET_PARTNERS_REQUEST':
         draft.status = 'REQUEST';
-
+        return draft;
       case 'GET_PARTNERS_SUCCESS':
         draft.status = 'SUCCESS';
         draft.items = action.payload;
-
+        return draft;
       case 'GET_PARTNERS_FAILURE':
         draft.status = 'FAILURE';
-
+        return draft;
       default:
         return draft;
     }
