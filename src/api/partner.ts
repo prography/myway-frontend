935e3a12bd9c3f2454074c4e0522fb4138115683
HeeYeon <gmldus0407@naver.com>
2020-05-05 13:18:59 +0900

    파트너들 axios get 타입 지정

--- src/api/partner.ts
+++ src/api/partner.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Partner } from 'models/partner';
 
 export const getPartners = async () => {
-  const { data } = await axios.get('https://api.copl.kr/partners');
+  const { data } = await axios.get<{ arr: Partner[] }>('https://api.copl.kr/partners');
 
-  return data.arr as Partner[];
+  return data.arr;
 };
