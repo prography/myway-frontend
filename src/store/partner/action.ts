cbe0fa791a07e997f7c631ba7b1d6f11e8693c42
HeeYeon <gmldus0407@naver.com>
2020-05-09 01:05:14 +0900

    파트너 신청 액션 생성

--- src/store/partner/action.ts
+++ src/store/partner/action.ts
@@ -2,10 +2,21 @@ import { createEntity } from 'utils/redux';
 import * as partnerApi from 'api/partner';
 
 export const GET_PARTNERS = 'GET_PARTNERS';
+export const APPLY_PARTNER = 'APPLY_PARTNER';
 
 export const getPartnersEntity = createEntity(GET_PARTNERS, partnerApi.getPartners);
 export const getPartners = () => ({
   type: GET_PARTNERS,
 });
 
+export const applyPartnerEntity = createEntity(
+  APPLY_PARTNER,
+  partnerApi.applyPartner,
+);
+export const applyPartner = (params: partnerApi.ApplyPartnerParams) => ({
+  type: APPLY_PARTNER,
+  params,
+});
+
 export type GetPartners = ReturnType<typeof getPartners>;
+export type ApplyPartners = ReturnType<typeof applyPartner>;
