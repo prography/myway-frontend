ac6e8a608d5f87af7908e268c3e565f576567c2d
HeeYeon <gmldus0407@naver.com>
2020-05-09 01:15:08 +0900

    파트너 신청 사가 생성

--- src/saga/partner.ts
+++ src/saga/partner.ts
@@ -1,13 +1,30 @@
-import { all, fork, takeLatest } from 'redux-saga/effects';
+import { all, fork, takeLatest, take, call } from 'redux-saga/effects';
 import { fetchEntity } from 'utils/saga';
-import { getPartnersEntity, GET_PARTNERS } from 'store/partner/action';
+import { 
+  getPartnersEntity, 
+  GET_PARTNERS,
+  applyPartnerEntity,
+  APPLY_PARTNER,
+  ApplyPartner,
+} from 'store/partner/action';
 
 const fetchGetPartners = fetchEntity(getPartnersEntity);
+const fetchApplyPartner = fetchEntity(applyPartnerEntity);
 
 function* watchGetPartners() {
   yield takeLatest(GET_PARTNERS, fetchGetPartners);
 }
 
+function* watchApplyPartner() {
+    while (true) {
+      const { params }: ApplyPartner = yield take(APPLY_PARTNER);
+      yield call(fetchApplyPartner, params);
+    }
+}
+
 export default function* partnerSaga() {
-  yield all([fork(watchGetPartners)]);
+  yield all([
+    fork(watchGetPartners),
+    fork(watchApplyPartner),
+  ]);
 }
