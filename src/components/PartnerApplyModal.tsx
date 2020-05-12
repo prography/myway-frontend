d9b4aa9e85a579dda15e8d9ba529493b8d2ec99e
HeeYeon <gmldus0407@naver.com>
2020-05-12 22:58:40 +0900

    파트너신청 칸 필수입력

--- src/components/PartnerApplyModal.tsx
+++ src/components/PartnerApplyModal.tsx
@@ -1,4 +1,5 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import styled from 'styled-components';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -29,7 +30,8 @@ const PartnerApplyModal: React.FC<Props> = ({ open, onClose }) => {
     setApplyContent({...applyContent, [e.currentTarget.id]: e.currentTarget.value});
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
     dispatch(applyPartner({
       phone: applyContent.phone,
       address: applyContent.address,
@@ -38,6 +40,13 @@ const PartnerApplyModal: React.FC<Props> = ({ open, onClose }) => {
       content: applyContent.content,
     }));
     window.alert('신청되었습니다 !');
+    setApplyContent({
+      phone: '',
+      address: '',
+      ownerName: '',
+      storeName: '',
+      content: '',
+    });
     onClose();
   };
   
@@ -46,57 +55,69 @@ const PartnerApplyModal: React.FC<Props> = ({ open, onClose }) => {
       <DialogTitle id="apply-partner" className={classes.title}>
         파트너 신청하기
       </DialogTitle>
-      <TextField 
-        id="ownerName" 
-        label="대표자명" 
-        variant="outlined" 
-        className={classes.textField} 
-        onChange={handleChange} 
-        value={applyContent.ownerName} 
-      />
-      <TextField 
-        id="storeName" 
-        label="가게 이름" 
-        variant="outlined" 
-        className={classes.textField} 
-        onChange={handleChange} 
-        value={applyContent.storeName} 
-      />
-      <TextField 
-        id="phone" 
-        label="연락처" 
-        variant="outlined" 
-        className={classes.textField}
-        onChange={handleChange} 
-        value={applyContent.phone} 
-      />
-      <TextField 
-        id="address" 
-        label="주소" 
-        variant="outlined" 
-        className={classes.textField}
-        onChange={handleChange} 
-        value={applyContent.address} 
-      />
-      <TextField 
-        id="content" 
-        label="전달사항" 
-        variant="outlined" 
-        multiline rows="3" 
-        className={classes.textField}
-        onChange={handleChange} 
-        value={applyContent.content} 
-      />
-      <DialogActions>
-        <Button onClick={handleSubmit} className={classes.button}>
-          신청하기
-        </Button>
-        <Button onClick={onClose} className={classes.button}>
-          닫기
-        </Button>
-      </DialogActions>
+      <Form onSubmit={handleSubmit}>
+        <TextField 
+          id="ownerName" 
+          label="대표자명" 
+          variant="outlined" 
+          className={classes.textField} 
+          onChange={handleChange} 
+          value={applyContent.ownerName} 
+          required={true} 
+        />
+        <TextField 
+          id="storeName" 
+          label="가게 이름" 
+          variant="outlined" 
+          className={classes.textField} 
+          onChange={handleChange} 
+          value={applyContent.storeName} 
+          required={true} 
+        />
+        <TextField 
+          id="phone" 
+          label="연락처" 
+          variant="outlined" 
+          className={classes.textField}
+          onChange={handleChange} 
+          value={applyContent.phone} 
+          required={true} 
+        />
+        <TextField 
+          id="address" 
+          label="주소" 
+          variant="outlined" 
+          className={classes.textField}
+          onChange={handleChange} 
+          value={applyContent.address} 
+          required={true} 
+        />
+        <TextField 
+          id="content" 
+          label="전달사항" 
+          variant="outlined" 
+          multiline rows="3" 
+          className={classes.textField}
+          onChange={handleChange} 
+          value={applyContent.content} 
+          required={true} 
+        />
+        <DialogActions>
+          <Button  type="submit" className={classes.button}>
+            신청하기
+          </Button>
+          <Button onClick={onClose} className={classes.button}>
+            닫기
+          </Button>
+        </DialogActions>
+      </Form>
     </Dialog>
   );
 }
 
 export default PartnerApplyModal;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
