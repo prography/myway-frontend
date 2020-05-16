75b3763c88004418e305ffda3e5aa9a8341cd395
HeeYeon <gmldus0407@naver.com>
2020-05-16 22:28:06 +0900

    페이지 라우팅 설정

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // lazy-load
 const MainPage = lazy(() => import('pages/MainPage'));
+const Login = lazy(() => import('pages/Auth/Login'));
+const Join = lazy(() => import('pages/Auth/Join'));
 
 const Root = () => (
   <Router>
     <Suspense fallback="loading...">
       <Switch>
         <Route exact path="/" component={MainPage} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/join" component={Join} />
       </Switch>
     </Suspense>
   </Router>
