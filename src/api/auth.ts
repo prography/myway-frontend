781de75b91eee79e49c4c178858d7579851d26e2
HeeYeon <gmldus0407@naver.com>
2020-05-17 04:19:24 +0900

    로그인, 회원정보 조회 api 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { User } from 'models/user';
+import { getAuthToken, setAuthToken } from 'utils/auth';
 
 export interface JoinParams {
   name: string;
@@ -11,4 +13,23 @@ export const joinAdvertiser = async (params: JoinParams) => {
   const { data } = await axios.post('https://api.copl.kr/advertisers',  params );
   
   return data;
+};
+
+export interface LoginParams {
+  email: string;
+  pwd: string;
+};
+  
+export const loginAdvertiser = async (params: LoginParams) => {
+  const { data } = await axios.post('https://api.copl.kr/advertisers/login',  params );
+  setAuthToken(data.data.token);
+
+  return data;
+};
+
+export const getUser = async (token?: string) => {
+  const headers = token ? { 'x-access-token': token } : {};
+  const { data } = await axios.get<{ data: User }>('https://api.copl.kr/advertisers/me', { headers });
+  
+  return data.data;
 };
