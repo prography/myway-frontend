fc75b4b334778ddd80e4d40f55aae8e2ac89e312
sanghup1234 <sanghup123@naver.com>
2020-06-07 18:13:14 +0900

    feat: container 반응형 작업

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -9,6 +9,11 @@ const CotainerWrap = styled.div`
   width: 100%;
   max-width: 1220px;
   margin: 0 auto;
+
+  @media screen and (max-width: 1440px) {
+    padding: 0 3rem;
+    max-width: 100%;
+  }
 `;
 
 function Container({ children }: Props) {
